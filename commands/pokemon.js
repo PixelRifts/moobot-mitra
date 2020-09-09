bb45d60ce0df911dc91cc7275c7e95ec15662d0c
Nikhil <njk777400@gmail.com>
2020-09-09 12:25:53 +0530

    Add Basic Files

---
+++ commands/pokemon.js
@@ -0,0 +1,19 @@
+const Pokedex = require('pokeapi-js-wrapper');
+const Discord = require('discord.js');
+const P = new Pokedex.Pokedex();
+
+module.exports = {
+    name: 'pokemon',
+	description: 'Get info of pokemon!',
+	execute(message, args) {
+        console.log(args);
+        try {
+            P.getPokemonByName(args[0]).then(function(response) {
+                message.channel.send('http://play.pokemonshowdown.com/sprites/ani/' + response.name + '.gif');
+            });
+        } catch (error) {
+            console.log(error);
+            message.reply('No such Pokemon found');
+        }
+	},
+}
---
+++ index.js
@@ -0,0 +1,33 @@
+const FileSystem = require('fs');
+const Discord = require('discord.js');
+const Client = new Discord.Client();
+Client.commands = new Discord.Collection();
+
+const commandFiles = FileSystem.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	Client.commands.set(command.name, command);
+}
+
+const Token = 'NzUyNTQwNjU5Nzg0NzQ1MDUw.X1ZIGg.aNhrXKpGL-yvy2qT-SaS7r8210A';
+const PREFIX = '/';
+
+Client.on('ready', () => {
+    console.log('MooBot is online! Say Hi everyone!!');
+});
+
+Client.on('message', message => {
+	if (!message.content.startsWith(PREFIX) || message.author.bot) return;
+
+	const args = message.content.slice(PREFIX.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+    try {
+        Client.commands.get(command).execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('there was an error trying to execute that command!');
+    }
+});
+
+Client.login(Token);
