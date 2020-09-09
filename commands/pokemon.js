3fe0b1a6cead4805369b10d1ca237392c91d8a5c
Nikhil <njk777400@gmail.com>
2020-09-09 13:10:36 +0530

    Really fix pokemon

--- commands/pokemon.js
+++ commands/pokemon.js
@@ -4,6 +4,6 @@ module.exports = {
     name: 'pokemon',
 	description: 'Get info of pokemon!',
 	execute(message, args) {
-        message.channel.send('http://play.pokemonshowdown.com/sprites/ani/' + response.name + '.gif');
+        message.reply('http://play.pokemonshowdown.com/sprites/ani/' + response.name + '.gif');
 	},
 }
