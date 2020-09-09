d5d6530ea2303c7684895aab431624be9c334d7d
Nikhil <njk777400@gmail.com>
2020-09-09 19:57:07 +0530

    Add Args an Pokemon abilities

--- commands/pokemon.js
+++ commands/pokemon.js
@@ -1,8 +1,43 @@
+Pokemon = require('pokeapi-js-wrapper');
+const Pokedex = new Pokemon.Pokedex();
+
+async function getPokemon(message, pokemonName) {
+    try {
+        return await Pokedex.getPokemonByName(pokemonName);
+    } catch (error) {
+        message.reply(`There was an error fetching Data for the Pokemon ${pokemonName}`);
+        return;
+    }
+}
+
+function showPokemon(message, pokemonName) {
+    message.channel.send('http://play.pokemonshowdown.com/sprites/ani/' + pokemonName + '.gif');
+}
+
+function listAbilities(message, pokemonName) {
+    getPokemon(message, pokemonName).then(pkmn => {
+        const abilities = pkmn.abilities;
+        message.reply(`Here are the abilities of the Pokemon ${pokemonName}`);
+        for (let a of abilities) {
+            let h = a.is_hidden;
+            var hidden_text;
+            if (h) hidden_text = ' [Hidden Ability]'
+            else hidden_text = '';
+            message.channel.send(a.ability.name + hidden_text);
+        }
+    });
+}
+
 module.exports = {
     name: 'pokemon',
-	description: 'Get info of pokemon!',
+    description: 'Get info of pokemon!',
+    args: true,
 	execute(message, args) {
-        console.log(args);
-        message.channel.send('http://play.pokemonshowdown.com/sprites/ani/' + args[0] + '.gif');
+        const pokemonName = args[0];
+        const operation = args[1];
+        switch (operation) {
+            case 'show': showPokemon(message, pokemonName); break;
+            case 'abilities': listAbilities(message, pokemonName); break;
+        }
 	}
 }
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const FileSystem = require('fs');
 const Discord = require('discord.js');
 const Client = new Discord.Client();
 Client.commands = new Discord.Collection();
-const DEV = false;
+const DEV = true;
 
 const commandFiles = FileSystem.readdirSync('./commands').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
@@ -20,13 +20,19 @@ Client.on('message', message => {
 	if (!message.content.startsWith(PREFIX) || message.author.bot) return;
 
 	const args = message.content.slice(PREFIX.length).trim().split(/ +/);
-	const command = args.shift().toLowerCase();
+    const commandName = args.shift().toLowerCase();
+    const command = Client.commands.get(commandName);
+    console.log(args);
+
+    if (command.args && !args.length) {
+    	return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+    }
 
     try {
-        Client.commands.get(command).execute(message, args);
+        command.execute(message, args);
     } catch (error) {
         console.error(error);
-        message.reply('there was an error trying to execute that command!');
+        message.reply('There was an error trying to execute that command!');
     }
 });
 
