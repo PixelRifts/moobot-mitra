09d694d21ce854482bb2ae1d5d64e4bbd83a2423
Nikhil <njk777400@gmail.com>
2020-09-19 17:14:07 +0530

    Add Pokeberry command

---
+++ commands/pokeberrry.js
@@ -0,0 +1,48 @@
+const Discord = require('discord.js');
+
+Pokemon = require('pokeapi-js-wrapper');
+const Pokedex = new Pokemon.Pokedex();
+
+module.exports = {
+    name: 'pokeberry',
+    description: 'Get info of Berries',
+    args: true,
+    cooldown: 5,
+    expected: '/pokeberry <Berry Name>',
+    execute(message, args) {
+        const berryName = args[0];
+        Pokedex.getBerryByName(berryName).then(berryResponse => {
+                let itemName = berryResponse.item.name;
+                Pokedex.getItemByName(itemName).then(berryItemResponse => {
+                        let effect_entries = berryItemResponse.effect_entries;
+                        var retval = ''; 
+                        for (let e of effect_entries) {
+                            if (e.language.name === 'en') {
+                                retval = e.effect;
+                            }
+                        }
+
+                        let flavors = berryResponse.flavors;
+                        var flavorString = '';
+                        for (let f of flavors) {
+                            flavorString = flavorString.concat(f.flavor.name).concat(' ');
+                        }
+                        flavorString = flavorString === '' ? 'NONE' : flavorString;
+
+                        let embed = new Discord.MessageEmbed()
+                                .setTitle(global.fixTitle(berryName + ' Berry'))
+                                .setImage(berryItemResponse.sprites.default)
+                                .addFields(
+                                    { name: 'Flavours', value: flavorString, inline: true },
+                                    { name: 'Firmness', value: global.fixTitle(berryResponse.firmness.name), inline: true },
+                                    
+                                    { name: 'Effect', value: retval },
+
+                                    { name: 'Natural Gift Power', value: berryResponse.natural_gift_power, inline: true },
+                                    { name: 'Natural Gift Type', value: global.capitalize(berryResponse.natural_gift_type.name), inline: true },
+                                );
+                        message.reply(embed);
+                });
+        });
+    }
+}
